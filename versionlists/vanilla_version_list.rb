155f168091d3b66c034a4577b32671863a556f65
Petr Mrázek <peterix@gmail.com>
2015-04-09 20:48:12 +0200

    NOISSUE always update latest release and snapshot minecraft jsons

--- versionlists/vanilla_version_list.rb
+++ versionlists/vanilla_version_list.rb
@@ -9,12 +9,15 @@ class VanillaVersionList < BaseVersionList
 
   def get_versions
     result = BaseVersionList.get_json 'http://s3.amazonaws.com/Minecraft.Download/versions/versions.json'
+    @latest_release = result[:latest][:release]
+    @latest_snapshot = result[:latest][:snapshot]
 
     return result[:versions].map do |obj| obj[:id] end
   end
 
   def get_version(id)
-    files = @input.parse BaseVersionList.get_json_cached('http://s3.amazonaws.com/Minecraft.Download/versions/' + id + '/' + id + '.json')
+    fun = (@latest_release == id or @latest_snapshot == id) ? BaseVersionList.method(:get_json) : BaseVersionList.method(:get_json_cached)
+    files = @input.parse fun.call('http://s3.amazonaws.com/Minecraft.Download/versions/' + id + '/' + id + '.json')
     mcfile = files.find do |file|
       file.uid == 'net.minecraft'
     end
