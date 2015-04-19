14c23255bc8fb4de7343a60f51fb924465d0d6b8
Jan Dalheimer <jan@dalheimer.de>
2015-04-19 18:28:11 +0200

    NOISSUE improve cache; add etag support

--- util/cache.rb
+++ util/cache.rb
@@ -9,6 +9,8 @@ class HTTPCatcher
   def initialize(basedir)
     @basedir = basedir
     FileUtils.mkdir_p @basedir unless Dir.exist? @basedir
+    @etags = {}
+    @etags = JSON.parse File.read(@basedir + '/etags.json') if File.exists? @basedir + '/etags.json'
   end
 
   # HTTP GETs a url if it doesn't exist locally
@@ -28,9 +30,9 @@ class HTTPCatcher
     cached_dir = File.dirname cached_path
     FileUtils.mkdir_p cached_dir unless Dir.exist? cached_dir
 
-    if should_check(cached_path, check_stale)
+    if should_check cached_path, check_stale
       puts "DL: #{url}"
-      resp = http_get(url, cached_path)
+      resp = http_get url, cached_path
       if resp == nil
         return
       end
@@ -42,74 +44,87 @@ class HTTPCatcher
 
   # get a file, using the local cached file modified timestamp to make sture we don't re-download stuff pointlessly
   # this also *should* handle redirection properly
-  def http_get(url, cached_path, limit = 10)
+  def http_get(url, cached_path, limit = 10, http = nil)
     # too many redirects...
     raise ArgumentError, 'too many HTTP redirects' if limit == 0
 
     uri = URI.parse(url)
-    head_req = Net::HTTP::Head.new(uri)
 
-    localDate = Time.parse("1985-10-28")
-    if File.exists?(cached_path)
-      file = File.stat cached_path
-      localDate = file.mtime
+    local_date = Time.parse("1985-10-28")
+    local_date = File.mtime cached_path if File.exists? cached_path
+
+    if http.nil?
+      Net::HTTP.start uri.hostname, uri.port, :use_ssl => uri.scheme == 'https' do |http|
+        return http_get_internal uri, cached_path, limit, http, local_date
+      end
+    else
+      return http_get_internal uri, cached_path, limit, http, local_date
     end
+  end
+  def http_get_internal(uri, cached_path, limit = 10, http = nil, local_date = nil)
+    existing_etag = @etags[uri]
 
-    head_resp = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') {|http|
-      http.request(head_req)
-    }
+    # start by doing a HEAD request
+    head_req = Net::HTTP::Head.new uri
+    head_req.add_field 'If-None-Match', existing_etag if existing_etag
+    head_req.add_field 'If-Modified-Since', local_date.httpdate
+    head_resp = http.request head_req
 
     case head_resp
     when Net::HTTPSuccess
-      checked(cached_path)
-      remoteDate = Time.httpdate(head_resp['Last-Modified'])
+      # don't re-check this
+      checked cached_path
+
+      remote_date = Time.httpdate(head_resp['Last-Modified'])
+      new_etag = head_resp['ETag']
+
       # if the remote resource has been modified later than the local file, grab it and return it
-      puts "Comparing #{localDate.httpdate} to #{remoteDate.httpdate}"
-      if(remoteDate > localDate)
+      puts "Comparing #{local_date.httpdate} to #{remote_date.httpdate}"
+      if remote_date > local_date && existing_etag != new_etag
         req = Net::HTTP::Get.new(uri)
-        resp = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') {|http|
-          http.request(req)
-        }
+        resp = http.request Net::HTTP::Get.new(uri)
         puts "GOT FULL FILE"
+
+        @etags[uri] = new_etag if new_etag
+        File.write @basedir + '/etags.json', JSON.generate(@etags)
+
         return resp
-      end
-      puts "CACHE HIT"
-      return nil # otherwise
-    when Net::HTTPRedirection
-      if head_resp.code == "304"
-        puts "THIS SHOULDN'T BE!"
-        checked(cached_path)
+      else
+        puts "CACHE HIT"
         return nil
       end
+    when Net::HTTPRedirection
+      if head_resp.code == "304"
+        puts "CACHE HIT"
+        checked cached_path
+        return nil
+      end
+
       location = head_resp['Location']
       puts "Redirected to #{location} - code #{head_resp.code}"
-      newurl=URI.parse(head_resp.header['location'])
-      if(newurl.relative?)
-        newurl=URI.join(url, head_resp.header['location'])
-      end
-      return http_get(newurl, cached_path, limit - 1)
+      newurl = URI.parse location
+      newurl = URI.join uri.to_s, location if newurl.relative?
+      return http_get newurl, cached_path, limit - 1, http
     else
       puts "Failed: #{head_resp.code}"
-      checked(cached_path)
+      checked cached_path
       return nil
     end
   end
 
+  @@checked_paths = Set.new
   def should_check(cached_path, check_stale)
     # if the file doesn't exist locally, or we should check for stale cache
-    if (not File.exist? cached_path) or check_stale
+    if !File.exist? cached_path or check_stale
       # but only once per run
-      return ($checked_paths == nil or not( $checked_paths.include? cached_path))
+      return !@@checked_paths.include?(cached_path)
     end
     # otherwise don't check
     return false
   end
 
   def checked(cached_path)
-    if $checked_paths == nil
-      $checked_paths = Set.new
-    end
-    $checked_paths.add cached_path
+    @@checked_paths.add cached_path
   end
 
   public
